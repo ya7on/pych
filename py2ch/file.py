d3438e630ddc38aee7907491b0da7f3e87b90b9c
Simon Suprun <simonsuprun@gmail.com>
2019-08-04 22:02:11 +0300

    Add File

--- py2ch/file.py
+++ py2ch/file.py
@@ -6,3 +6,18 @@ class File(object):
 
     def __init__(self, **kwargs):
         """Parse file info data."""
+        self.displayname = kwargs['displayname']
+        self.duration = kwargs['duration']
+        self.duration_secs = kwargs['duration_secs']
+        self.fullname = kwargs['fullname']
+        self.height = kwargs['height']
+        self.md5 = kwargs['md5']
+        self.name = kwargs['name']
+        self.nsfw = kwargs['nsfw']
+        self.path = kwargs['path']
+        self.size = kwargs['size']
+        self.thumbnail = kwargs['thumbnail']
+        self.tn_height = kwargs['tn_height']
+        self.tn_width = kwargs['tn_width']
+        self.type = kwargs['type']
+        self.width = kwargs['width']
